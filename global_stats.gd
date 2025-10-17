791857d366003b14cdf25d73ff503131903b2738
Jeffrey Yeung <jyeungprivate@gmail.com>
2025-10-17 16:34:38 -0400

    basic asteroid spawning

---
+++ global_stats.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@export var health: float = 0.0
+@export var money: int = 0
+
+enum penColor {red, blue, green}
---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var rng = RandomNumberGenerator.new()
+var currentAsteroid
+@onready var asteroidScene: PackedScene = preload("res://Space Objects/Asteroid.tscn")
+
+func _ready() -> void:
+	rng.randomize()
+	currentAsteroid = asteroidScene
+	spawnAsteroids()
+
+func spawnAsteroids():
+	while true:
+		var asteroid := asteroidScene.instantiate() as Asteroid
+		asteroid.global_position = Vector2(rng.randf_range(0.0, 1250.0), rng.randf_range(-250.0, 0.0))
+		var spread = deg_to_rad(20.0)
+		asteroid.dir = Vector2.DOWN.rotated(rng.randf_range(-spread, spread))
+		add_child(asteroid)
+		await get_tree().create_timer(2).timeout

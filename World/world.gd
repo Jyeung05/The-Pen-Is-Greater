c2653c822bb4fdba8083dfeb6ca45046f92f3974
Clement Quach <clementcnd@gmail.com>
2025-10-19 13:50:05 -0700

    removed restart

--- World/world.gd
+++ World/world.gd
@@ -8,9 +8,9 @@ func _ready() -> void:
 	pass
 	
 	
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("restart"):
-		GlobalStats.restart()
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("restart"):
+		#GlobalStats.restart()
 
 func goal() -> void:
 	GlobalStats.anteCountDown = 30

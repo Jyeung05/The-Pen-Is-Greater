83145d63734a1d5a5ac6018da198a064f834fa0b
Clement Quach <clementcnd@gmail.com>
2025-10-19 13:56:02 -0700

    fixed spawn time reduction

--- stat_card.gd
+++ stat_card.gd
@@ -58,7 +58,7 @@ func _on_plus() -> void:
 		if level >= max_level:
 			return
 		level +=1
-		GlobalStats.set(global_var, GlobalStats.get(global_var) - 0.2)
+		GlobalStats.set(global_var, GlobalStats.get(global_var) * 0.6)
 		GlobalStats.money -= upgradeCost
 		upgradeCost = upgradeCost * upgradeCostMultiplyer
 		plus_btn.text = "Upgrade Cost: $" + str(upgradeCost)

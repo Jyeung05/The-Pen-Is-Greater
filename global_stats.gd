239b1b0349f8c0f56893fd04b2bb3555912f02c3
Jeffrey Yeung <jyeungprivate@gmail.com>
2025-10-19 13:36:50 -0400

    restart screen

--- global_stats.gd
+++ global_stats.gd
@@ -32,3 +32,43 @@ var currentPen = penType.basic
 
 @export var spawnCooldown: float = 1.0
 @export var asteroidWorth: int = 1
+
+# RESTART VARIABLES
+
+var Rhealth: float = health
+var Rmoney: int = money
+var Rgravity: float = gravity
+var Rante: int = ante
+var RbasicUnlocked: bool = basicUnlocked
+var RbounceUnlocked: bool = bounceUnlocked
+var RphaseUnlocked: bool = phaseUnlocked
+var RprofitUnlocked: bool = profitUnlocked
+var RbasicEnergyMax: int = basicEnergyMax
+var RbounceEnergyMax: int = bounceEnergyMax
+var RphaseEnergyMax: int = phaseEnergyMax
+var RprofitEnergyMax: int = profitEnergyMax
+var Rdistrabution:Array = distrabution
+var ReventList:Array = eventList
+var RbuffList:Array = buffList
+var RspawnCooldown: float = spawnCooldown
+var RasteroidWorth: int = asteroidWorth
+
+
+func restart():
+	health = Rhealth
+	money = Rmoney
+	gravity = Rgravity
+	ante = Rante
+	basicUnlocked = RbasicUnlocked
+	bounceUnlocked = RbounceUnlocked
+	phaseUnlocked = RphaseUnlocked
+	profitUnlocked = RprofitUnlocked
+	basicEnergyMax = RbasicEnergyMax
+	bounceEnergyMax = RbounceEnergyMax
+	phaseEnergyMax = RphaseEnergyMax
+	profitEnergyMax = RprofitEnergyMax
+	distrabution = Rdistrabution
+	eventList = ReventList
+	buffList = RbuffList
+	spawnCooldown = RspawnCooldown
+	asteroidWorth = RasteroidWorth
